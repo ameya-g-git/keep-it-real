36e12cd2ea879fdf2b54d7e023dc3f57a9fe03d8
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-05-04 11:41:54 -0400

    fixed font for various platforms

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@
 module.exports = {
   content: ["./frontend/index.html"],
   theme: {
-    fontFamily: {
-      sans: ["Tahoma"]
-    },
     extend: {
       colors: {
         background: "#F2F2F7",
